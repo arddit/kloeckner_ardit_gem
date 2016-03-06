f7b36d4e385a4062ec686b6bc1956f71245885a3
Ardi <ardihamza@gmail.com>
2016-03-07 00:47:46 +0100

    fixing errors

--- kloeckner_ardit-0.0.0/lib/kloeckner_ardit/dictionary.rb
+++
@@ -1,7 +0,0 @@
-module KloecknerArdit
-	class Dictionary
-		def self.get
-			File.read('/usr/share/dict/words').lines.select {|l| (4..9).cover?(l.strip.size)}.sample.strip
-		end
-	end
-end
--- kloeckner_ardit-0.0.0/spec/spec_helper.rb
+++
@@ -1,1 +0,0 @@
-require 'kloeckner_ardit'
